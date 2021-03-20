8244df36ea2b84d11da4ef9d8d5ec7149086ddd3
Andrew Irving <73566194+irvingam@users.noreply.github.com>
2021-03-20 17:29:45 -0400

    added function that creates weather modules

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
-// Selectors
+                // Selectors //
 const api = {
     key: "dd27bad266e3ddbf9645ca1c6f57f7e5",
     base: "https://api.openweathermap.org/data/2.5/"
 }
+const addBtn = document.getElementById('add-btn')
+const input = document.getElementById('search')
 
-// Events
+                // Events //
+addBtn.addEventListener('click', setQuery)
 
-// Functions
+// displays current date
+window.onload = function displayDate() {
+    let now = new Date()
+    let currentDate = document.getElementById('current-date')
+    currentDate.innerText = getDate(now)
+}
+
+                // Functions //
+function setQuery(event) {
+    getResults(input.value)
+}
+
+function getDate(date) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+    let month = months[date.getMonth()]
+    let day = days[date.getDay()]
+    let today = date.getDate()
+    let year = date.getFullYear()
+
+    return `${day} ${today} ${month} ${year}`
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json()
+    }).then(displayWeather)
+}
+
+function displayWeather(weather) {
+    console.log(weather)
+
+    const module = document.createElement('main')
+    module.classList.add('module')
+
+    const location = document.createElement('section')
+    location.classList.add('location')
+    module.appendChild(location)
+
+    const city = document.createElement('div')
+    city.innerText = `${weather.name} ${weather.sys.country}`
+    city.classList.add('city')
+    location.appendChild(city)
+
+    const current = document.createElement('section')
+    current.classList.add('current')
+    module.appendChild(current)
+
+    const temp = document.createElement('div')
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`
+    temp.classList.add('temp')
+    current.appendChild(temp)
+
+    const weatherCurrent = document.createElement('div')
+    weatherCurrent.innerText = weather.weather[0].main
+    weatherCurrent.classList.add('weather')
+    current.appendChild(weatherCurrent)
+
+    const hilow = document.createElement('div')
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`
+    hilow.classList.add('hi-low')
+    current.appendChild(hilow)
+
+    const moduleContainer = document.querySelector('.module-container')
+    moduleContainer.appendChild(module)
+
+}
